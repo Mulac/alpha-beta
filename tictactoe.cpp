0a8fff6c11a75f1464cc181dd646b7d19181d9d5
wallbridge.calum@gmail.com <wallbridge.calum@gmail.com>
2020-05-26 17:52:34 +0100

    implementation

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,7 +1,45 @@
 #include "tictactoe.hpp"
 
+#include <algorithm>
+
 const int* TicTacToe::getBoard() const {
     return board;
+}
+
+const std::list<int>& TicTacToe::legalActions() const {
+    std::list<int> actions = {};
+    const int* board = getBoard();
+    for (int i=0; i<9; i++){
+        if (!board[i])
+            actions.push_back(i);
+    }
+    return actions;
+}
+
+int TicTacToe::makeMove(int move) {
+    // Check move is legal
+    auto actions = legalActions();
+    if (std::find(actions.begin(), actions.end(), move) == actions.end())
+        return -1;
+    
+    // Make move and switch player
+    board[move] = player;
+    player *= -1;
+}
+
+bool TicTacToe::hasWon(int player) const {
+    for (int i = 0; i < 9; i++)
+        for (int j = 0; j < 9; j++)
+            for (int k = 0; k < 9; k++)
+                if (i != j && i != k && j != k)
+                    if (board[i] == player && board[j] == player && board[k] == player)
+                        if (magic[i] + magic[j] + magic[k] == 15)
+                            return true;
+    return false;
+}
+
+bool TicTacToe::isEnd() const {
+    return (legalActions().empty()) ? true : false;
 }
 
 char toSym(int player) {
--- tictactoe.hpp
+++ tictactoe.hpp
@@ -2,6 +2,7 @@
 #define TICTACTOE_H_
 
 #include <iostream>
+#include <list>
 
 class TicTacToe {
 public:
@@ -9,14 +10,15 @@ public:
     bool isEnd() const;
     int winner() const;
     int makeMove(int move);
-    const int* legalActions() const;
+    const std::list<int>& legalActions() const;
     const int* getBoard() const;
 
 private:
     short player;
     int board[9] = {};
+    int magic[9] = {2, 7, 6, 9, 5, 1, 4, 3, 8};
 
-    
+    bool hasWon(int player) const;
 };
 
 std::ostream& operator<< (std::ostream& output, const TicTacToe& game);
