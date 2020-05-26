4dbf3d0c89d81e96bc9d46a2accc5b1d0736b1f9
wallbridge.calum@gmail.com <wallbridge.calum@gmail.com>
2020-05-26 17:22:13 +0100

    ostream overloading

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -1,8 +1,24 @@
 #include "tictactoe.hpp"
 
-#include <iostream>
+const int* TicTacToe::getBoard() const {
+    return board;
+}
+
+char toSym(int player) {
+    return (player > 0) ? 'X' : (player < 0) ? 'O' : '-';
+}
+
+std::ostream& operator<< (std::ostream& output, const TicTacToe& game){
+    const int* board = game.getBoard();
+
+    output << toSym(board[0]) << ' ' << toSym(board[1]) << ' ' << toSym(board[2]) << std::endl;
+    output << toSym(board[3]) << ' ' << toSym(board[4]) << ' ' << toSym(board[5]) << std::endl;
+    output << toSym(board[6]) << ' ' << toSym(board[7]) << ' ' << toSym(board[8]) << std::endl;
+
+    return output;
+}
 
 int main(){
     TicTacToe t;
-    //std::cout << t << std::endl;
+    std::cout << t << std::endl;
 }
--- tictactoe.hpp
+++ tictactoe.hpp
@@ -1,17 +1,24 @@
 #ifndef TICTACTOE_H_
 #define TICTACTOE_H_
 
+#include <iostream>
+
 class TicTacToe {
 public:
     TicTacToe() : player(1) {}
     bool isEnd() const;
     int winner() const;
-    int makeMove(int x, int y);
+    int makeMove(int move);
+    const int* legalActions() const;
     const int* getBoard() const;
 
 private:
     short player;
     int board[9] = {};
+
+    
 };
+
+std::ostream& operator<< (std::ostream& output, const TicTacToe& game);
 
 #endif
